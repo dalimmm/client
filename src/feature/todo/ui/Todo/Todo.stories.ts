d8df24658988d23395e70c896552e766002ad593
njh0317 <njnjh0317@gmail.com>
2024-03-31 19:08:09 +0900

    [Feat] Add Todo Component

--- src/feature/todo/ui/Todo/Todo.stories.ts
+++ src/feature/todo/ui/Todo/Todo.stories.ts
@@ -14,12 +14,16 @@ type Story = StoryObj<typeof Todo>;
 export const Default: Story = {
   args: {
     content: 'This is todo',
+    done: false,
+    due: 1711875600,
+    dueVisible: true,
   },
 };
 
 export const Done: Story = {
   args: {
-    done: true,
     content: 'This is todo',
+    done: true,
+    due: 1711875600,
   },
 };
--- src/feature/todo/ui/Todo/Todo.tsx
+++ src/feature/todo/ui/Todo/Todo.tsx
@@ -1,14 +1,28 @@
+import { DateTime } from 'luxon';
+import { Checkbox } from '~/base/components/ui/checkbox';
+
 export function Todo({
   content,
-  done = false,
+  done,
+  due,
+  dueVisible = false,
 }: {
   content: string;
   done: boolean;
+  due: number;
+  dueVisible?: boolean;
 }) {
   return (
-    <div>
-      <input type="checkbox" checked={done} />
-      {content}
+    <div className="flex items-center p-2 justify-between">
+      <div className="flex items-center">
+        <Checkbox className="mr-2" checked={done} />
+        <span className="font-semibold">{content}</span>
+      </div>
+      {dueVisible ? (
+        <div className="font-light text-sm">
+          {DateTime.fromSeconds(due).toFormat('yyyy MM dd')}
+        </div>
+      ) : null}
     </div>
   );
 }
